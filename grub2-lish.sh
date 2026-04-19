00b1c8303db89289450fac50f72c3cc1d26d48ae
antimomentum <71552326+antimomentum@users.noreply.github.com>
2026-04-19 14:32:22 -0400

    Update grub2-lish.sh

--- grub2-lish.sh
+++ grub2-lish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script assumes the img has been copied to an ext4 disk and using the grub file supplied with Linode Lish settings
+# Run update-grub from the raw disk first, in Rescue mode copy from raw /dev/sda1 to the ext4 /dev/sdX disk
 UUID=$(blkid | awk '{print $2}' | sed 's/UUID="//; s/"//')
 sed -i -e "s|\--fs-uuid --set=root $UUID|\--set=root /dev/sda|g" /boot/grub/grub.cfg
 sed -i -e "s|root=UUID=$UUID|root=/dev/sda|g" /boot/grub/grub.cfg
