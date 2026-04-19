95b867e22346c3ae067e5d1b990c02402f33dc6d
antimomentum <71552326+antimomentum@users.noreply.github.com>
2026-04-19 14:33:58 -0400

    Update grub2-lish.sh

--- grub2-lish.sh
+++ grub2-lish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Run update-grub from the raw disk first, in Rescue mode copy from raw /dev/sda1 to the ext4 /dev/sdX disk
+# Run this script once without any extra disks, not even swap!
 UUID=$(blkid | awk '{print $2}' | sed 's/UUID="//; s/"//')
 sed -i -e "s|\--fs-uuid --set=root $UUID|\--set=root /dev/sda|g" /boot/grub/grub.cfg
 sed -i -e "s|root=UUID=$UUID|root=/dev/sda|g" /boot/grub/grub.cfg
